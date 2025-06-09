682c8b606f02c96dc0151ae680f73f4d7a403e54
carolinef158 <cfurmanski158@gmail.com>
2025-06-09 18:41:20 -0400

    Added all hardware constants

--- src/main/java/frc/team10505/robot/Constants.java
+++ src/main/java/frc/team10505/robot/Constants.java
@@ -3,11 +3,42 @@ package frc.team10505.robot;
 public class Constants {
     public class HardwareConstants{
         //Can ids. Id's 40-51 are used in the drivetrain.
-        public final static int ALGAE_PIVOT_MOTOR_ID = 0;//TODO assign value
+         /*Robo Rio ports/channels */
+    public final static int DRIVETRAIN_BLINKY_LIGHT_CHANNEL = 0;
 
-        //Current limits
-        public final static double CORAL_MOTOR_CURRENT_LIMIT = 0;//TODO assign value
-        //TODO add all other devices (burgle values from season code)
+    /*CAN IDS */
+    public final static int CORAL_LEFT_MOTOR_ID = 2;
+    public final static int CORAL_RIGHT_MOTOR_ID = 3;
+
+    public final static int ALGAE_INTAKE_MOTOR_ID = 7;
+    public final static int ALGAE_PIVOT_MOTOR_ID = 8;
+
+    public final static int ELEVATOR_MOTOR_ID = 10;
+    public final static int ELEVATOR_FOLLOWER_MOTOR_ID = 11;
+
+    /*40-51 can ids are used in the drivetrain */
+
+    public final static int DRIVETRAIN_RIGHT_LASER_ID = 52;
+    public final static int DRIVETRAIN_LEFT_LASER_ID = 53;
+   
+    public final static int CORAL_IN_LASER_ID = 60;
+    public final static int CORAL_OUT_LASER_ID = 61;
+
+    /*Current Limits */   
+    public final static int ALGAE_PIVOT_MOTOR_CURRENT_LIMIT = 15;
+    public final static int ALGAE_INTAKE_MOTOR_CURRENT_LIMIT = 25;
+
+    public final static int CORAL_MOTOR_CURRENT_LIMIT = 15;
+
+    public final static int ELEVATOR_MOTOR_CURRENT_LIMIT = 40;
+
+    /*Encoder Offsets */
+    public final static double ALGAE_PIVOT_ENCODER_OFFSET = 0;
+    public final static double ALGAE_PIVOT_ENCODER_SCALE = 360;
+
+    /*Gearstacks */
+    public final static double ELEVATOR_GEARSTACK = 12;
+    public final static double ALGAE_PIVOT_GEARSTACK = 80;
     }
 
     public class AlgaeConstants{
