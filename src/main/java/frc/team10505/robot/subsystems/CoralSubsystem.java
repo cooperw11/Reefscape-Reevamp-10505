5213cf5548e03d6ed3b9d05d72ad80cb853dd383
carolinef158 <cfurmanski158@gmail.com>
2025-06-10 09:39:39 -0400

    Cleaning coral and starting simulation
    in coral subsys - move motor id/current limits to constants and other cleaning (+add instructions). in simulation- creating vars of our subsystems

--- src/main/java/frc/team10505/robot/subsystems/CoralSubsystem.java
+++ src/main/java/frc/team10505/robot/subsystems/CoralSubsystem.java
@@ -1,6 +1,5 @@
 package frc.team10505.robot.subsystems;
 
-import com.ctre.phoenix6.Utils;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -8,35 +7,21 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkMax;
 
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.math.system.plant.LinearSystemId;
-import edu.wpi.first.wpilibj.simulation.FlywheelSim;
-import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
-import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
-import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
-import edu.wpi.first.wpilibj2.command.Command;
+import static frc.team10505.robot.Constants.HardwareConstants.*;
+import static frc.team10505.robot.Constants.CoralConstants.*;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 //TODO import hardware constants AND coral constants, statically (instead of "import", write "import static" and before the semicolon, add ".*" to import all the vars in the class)
 
 public class CoralSubsystem extends SubsystemBase {
+        // Variables (Motors)
+        private SparkMax flywheelRight = new SparkMax(CORAL_RIGHT_MOTOR_ID, MotorType.kBrushless);
+        private SparkMax flywheelLeft = new SparkMax(CORAL_LEFT_MOTOR_ID, MotorType.kBrushless);
 
-        // Konstants
-        private static final int kIntakeLeft = 1;//TODO move to constants
-        private static final int kIntakeRight = 2;//TODO move to constants
-
-        public static final int CORAL_MOTOR_CURRENT_LIMIT = 30;//TODO move to constants
-
-        // Mystery Stuph defining
+        // Mystery Stuph defining (Configurators)
         private SparkMaxConfig coralLeftConfig = new SparkMaxConfig();
         private SparkMaxConfig coralRightConfig = new SparkMaxConfig();
-        
-
-        // Variables
-        private SparkMax flywheelRight = new SparkMax(kIntakeRight, MotorType.kBrushless);
-        private SparkMax flywheelLeft = new SparkMax(kIntakeLeft, MotorType.kBrushless);                                                         
-                                                                                
-
 
         // Konstructor
         public CoralSubsystem() {
@@ -47,10 +32,9 @@ public class CoralSubsystem extends SubsystemBase {
                 flywheelLeft.configure(coralLeftConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
                 flywheelRight.configure(coralRightConfig, ResetMode.kResetSafeParameters,
                                 PersistMode.kPersistParameters);
-
-                //TODO add smartdashboard thing to put mechanism2d (reference personal project if needed)
-
         }
 
+        //TODO add methods
 
+        //TODO add periodic method(Should only put stuff to Smartdashboard)
 }
