768ba10794647e7c6e3fa66473e0af7c2f72ed98
giveItTheBeanz <rqschmidt2000@gmail.com>
2025-06-11 18:54:26 -0400

    Revert "Algae Flywheel Done"
    This reverts commit 0dfab17cd374f6734c59a095adcb2ec1d757dcd8.

--- src/main/java/frc/team10505/robot/RobotContainer.java
+++ src/main/java/frc/team10505/robot/RobotContainer.java
@@ -9,105 +9,121 @@ import static edu.wpi.first.units.Units.RotationsPerSecond;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
-
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-
 import frc.team10505.robot.subsystems.AlgaeSubsystem;
+import frc.team10505.robot.subsystems.ElevatorSubsystem;
 import frc.team10505.robot.subsystems.CoralSubsystem;
 import frc.team10505.robot.subsystems.drive.DrivetrainSubsystem;
 import frc.team10505.robot.subsystems.drive.generated.TunerConstants;
 
-
-
-public class RobotContainer { 
-
+public class RobotContainer {
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond);
-    
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond);
 
-    private SwerveRequest.RobotCentric robotDrive = new SwerveRequest.RobotCentric().withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+    private SwerveRequest.RobotCentric robotDrive = new SwerveRequest.RobotCentric()
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
-    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric().withDeadband(MaxSpeed*0.1).withRotationalDeadband(MaxAngularRate*0.1).withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric().withDeadband(MaxSpeed * 0.1)
+            .withRotationalDeadband(MaxAngularRate * 0.1).withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
     private final Telemetry logger = new Telemetry(MaxSpeed);
 
-    /*controllers */
+    /* controllers */
     private CommandJoystick joystick;
     private CommandJoystick joystick2;
 
-    private CommandXboxController xbox = new CommandXboxController(0);
+    private CommandXboxController driveController = new CommandXboxController(0);
     private CommandXboxController xbox2;
 
-    /*Subsystems */
-    private final DrivetrainSubsystem drivetrainSubsystem = TunerConstants.createDrivetrain();
-    // private final ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
-    private final AlgaeSubsystem algaeSubsystem = new AlgaeSubsystem();
-    private final CoralSubsystem coralSubsystem = new CoralSubsystem();
+    /* Subsystems */
+    private final DrivetrainSubsystem drivetrainSubsys = TunerConstants.createDrivetrain();
+    private final ElevatorSubsystem elevatorSubsys = new ElevatorSubsystem();
+    private final AlgaeSubsystem algaeSubsys = new AlgaeSubsystem();
+    private final CoralSubsystem coralSubsys = new CoralSubsystem();
 
-    /*Sendable choosers */
-    private SendableChooser<Double> polarityChooser = new SendableChooser<>(); 
-    //TODO add sendable chooser <Command> for autons. Dont assign it a value though
+    //TODO add other classes(Simulation and Superstructure)
 
+    /* Sendable choosers */
+    private SendableChooser<Double> polarityChooser = new SendableChooser<>();
+    // TODO add sendable chooser <Command> for autons. Dont assign it a value though
 
-    /*Constructor */
-    public RobotContainer(){
-        if(RobotBase.isSimulation()){
+    /* Constructor */
+    public RobotContainer() {
+        if (RobotBase.isSimulation()) {
             joystick = new CommandJoystick(0);
             joystick2 = new CommandJoystick(1);
 
             simConfigButtonBindings();
         } else {
-            xbox = new CommandXboxController(0);
+            driveController = new CommandXboxController(0);
             xbox2 = new CommandXboxController(1);
 
             configButtonBindings();
         }
 
-        //TODO call autobuilder config once created
+        // TODO call autobuilder config once created
         configNamedCommands();
         configSendableChoosers();
     }
 
-    private void simConfigButtonBindings(){
+    //TODO create(& call) sim config default command method (just for drivetrain subsys, reference season code if need be)
 
+    //TODO create(& call) config default command method (just for drivetrain subsys, reference season code if need be)
+
+
+    private void simConfigButtonBindings(){
+        //TODO add stuff
     }
 
-    private void configButtonBindings(){
-        if (Utils.isSimulation()) {
-            joystick.button(1).onTrue(algaeSubsystem.setAngle(0));
-            joystick.button(2).onTrue(algaeSubsystem.setAngle(-30));
-            joystick.button(3).onTrue(algaeSubsystem.setAngle(-90));
+    private void configButtonBindings() {
+        if (Utils.isSimulation()) {//TODO get rid of if/else statement. Move joystick bindings to simConfigButtonBindings method
+            joystick.button(1).onTrue(algaeSubsys.setAngle(0));
+            joystick.button(2).onTrue(algaeSubsys.setAngle(-30));
+            joystick.button(3).onTrue(algaeSubsys.setAngle(-90));
         } else {
-            xbox.x().onTrue(algaeSubsystem.setAngle(0));
-            xbox.a().onTrue(algaeSubsystem.setAngle(-35));
-            xbox.b().onTrue(algaeSubsystem.setAngle(-90));
-
-            xbox.leftBumper().whileTrue(algaeSubsystem.runIntake(.6));
-            xbox.rightBumper().whileTrue(algaeSubsystem.runIntake(-.6));
+            driveController.x().onTrue(algaeSubsys.setAngle(0));
+            driveController.a().onTrue(algaeSubsys.setAngle(-30));
+            driveController.b().onTrue(algaeSubsys.setAngle(-90));
+            driveController.y().onTrue(algaeSubsys.setAngle(90));
         }
 
-         xbox.povUp().whileTrue(drivetrainSubsystem.applyRequest(() -> robotDrive.withVelocityX(0.0).withVelocityY(0.6).withRotationalRate(0.0))).onFalse(drivetrainSubsystem.stop());
-         xbox.povDown().whileTrue(drivetrainSubsystem.applyRequest(() -> robotDrive.withVelocityX(0.4).withVelocityY(0.0).withRotationalRate(0.0))).onFalse(drivetrainSubsystem.stop());
-         xbox.povLeft().whileTrue(drivetrainSubsystem.applyRequest(() -> robotDrive.withVelocityX(0.0).withVelocityY(0.6).withRotationalRate(0.0)).until(() -> !drivetrainSubsystem.seesLeftSensor()));
-         xbox.povRight().whileTrue(drivetrainSubsystem.applyRequest(() -> robotDrive.withVelocityX(0.0).withVelocityY(-0.6).withRotationalRate(0.0)).until(() -> !drivetrainSubsystem.seesRightSensor())); 
+        //TODO double check directions (pov up should be a negative velocity x, I think)
+        driveController.povUp()
+                .whileTrue(drivetrainSubsys
+                        .applyRequest(() -> robotDrive.withVelocityX(0.0).withVelocityY(0.6).withRotationalRate(0.0)))
+                .onFalse(drivetrainSubsys.stop());
+        driveController.povDown()
+                .whileTrue(drivetrainSubsys
+                        .applyRequest(() -> robotDrive.withVelocityX(0.4).withVelocityY(0.0).withRotationalRate(0.0)))
+                .onFalse(drivetrainSubsys.stop());
+        driveController.povLeft()
+                .whileTrue(drivetrainSubsys
+                        .applyRequest(() -> robotDrive.withVelocityX(0.0).withVelocityY(0.6).withRotationalRate(0.0))
+                        .until(() -> !drivetrainSubsys.seesLeftSensor()));
+        driveController.povRight()
+                .whileTrue(drivetrainSubsys
+                        .applyRequest(() -> robotDrive.withVelocityX(0.0).withVelocityY(-0.6).withRotationalRate(0.0))
+                        .until(() -> !drivetrainSubsys.seesRightSensor()));
+
     }
 
-    private void configSendableChoosers(){
+    private void configSendableChoosers() {
         polarityChooser.setDefaultOption("Positive", 1.0);
         polarityChooser.addOption("Negative", -1.0);
         SmartDashboard.putData("Polarity Chooser", polarityChooser);
 
     }
 
-    private void configNamedCommands(){
-
+    private void configNamedCommands() {
+        //TODO add stuff here
     }
-     // Pivot Controls
-     private void algaePivotControls() {
-        
+
+    // Pivot Controls
+    private void algaePivotControls() {
+
     }
 }
